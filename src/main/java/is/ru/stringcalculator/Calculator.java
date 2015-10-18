2822f18400e68e34375ed9cdce2913d6e5066c8c
Magnea Hilmarsdóttir <kristjanah13@ru.is>
2015-10-18 16:20:36 +0000

    added test one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,24 +1,23 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
-
+/*
 	public static int add(String text){
 		return 0;
 	}
-}
-	/*
-
+*/
 	public static int add(String text){
-		if(text.equals("")){
-			return 0;
+		if(text.equals("")) /*{ */
+			return 0; /*
 		}
 		else if(text.contains(",")){
 			return sum(splitNumbers(text));
-		}
+		} */
 		else
 			return 1;
 	}
-
+}
+/*
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
