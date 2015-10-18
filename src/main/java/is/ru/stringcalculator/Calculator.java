29ed7d48cce283cea567952cf7cd20a704c10622
Magnea Hilmarsdóttir <kristjanah13@ru.is>
2015-10-18 20:20:03 +0000

    added test two integers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,12 +7,13 @@ public class Calculator {
 	}
 */
 	public static int add(String text){
-		if(text.equals("")) /*{ */
-			return 0; /*
+		if(text.equals("")) { 
+			return 0; 
 		}
 		else if(text.contains(",")){
-			return sum(splitNumbers(text));
-		} */
+			Sting[]numbers = text.split(",");
+			return Integer.parseInt(numbers[0] + Integer.parseInt(numbers[2]);
+		} 
 		else
 			return 1;
 	}
